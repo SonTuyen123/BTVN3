160ebe47e8b81d66b6b0c0404dc54f8fab2c2bd2
Admin <hanhngao@gmail.com>
2022-05-24 16:28:08 +0700

    Initial commit

---
+++ app/src/main/java/com/caotuyen/btvn3/MainActivity.java
@@ -0,0 +1,17 @@
+package com.caotuyen.btvn3;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Toolbar;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+}
